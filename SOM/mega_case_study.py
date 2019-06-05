ea0d6e2cda7c60de67fb6d0f181c417e33fc69d4
Sakshamdeep Singh <saksham.sds@gmail.com>
2019-06-05 17:20:08 +0530

    Update mega_case_study.py

--- SOM/mega_case_study.py
+++ SOM/mega_case_study.py
@@ -1,56 +1,53 @@
-# Mega Case Study - Make a Hybrid Deep Learning Model
+# Mega Case Studies - Hybrid Deep Learning Model
 
-
-
-# Part 1 - Identify the Frauds with the Self-Organizing Map
+# Part 1 - Identifying frauds using Self-Organising Maps
 
 # Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Credit_Card_Applications.csv')
+dataset = pd.read_csv("Credit_Card_Applications.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
+sc = MinMaxScaler(feature_range=(0,1))
 X = sc.fit_transform(X)
 
 # Training the SOM
 from minisom import MiniSom
-som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som = MiniSom(x=10, y=10, input_len=15, sigma=1, learning_rate=0.5)
 som.random_weights_init(X)
-som.train_random(data = X, num_iteration = 100)
+som.train_random(X, num_iteration=100)
 
 # Visualizing the results
-from pylab import bone, pcolor, colorbar, plot, show
+# Higher the Mean Inter-node Distance, implies more the outlier
+from pylab import bone, pcolor, plot, show, colorbar
 bone()
 pcolor(som.distance_map().T)
 colorbar()
 markers = ['o', 's']
-colors = ['r', 'g']
+colors = ['r','g']
 for i, x in enumerate(X):
     w = som.winner(x)
-    plot(w[0] + 0.5,
-         w[1] + 0.5,
+    plot(w[0]+0.5,                      #putting marker in the center of winning node
+         w[1]+0.5,
          markers[y[i]],
          markeredgecolor = colors[y[i]],
          markerfacecolor = 'None',
          markersize = 10,
          markeredgewidth = 2)
-show()
+show()  
 
-# Finding the frauds
+# Finding the frauds 
 mappings = som.win_map(X)
-frauds = np.concatenate((mappings[(5,3)], mappings[(8,3)]), axis = 0)
+frauds = np.concatenate((mappings[(1,6)], mappings[(5,3)]), axis = 0)
 frauds = sc.inverse_transform(frauds)
+frauds = frauds.astype(np.int64)
 
-
-
-# Part 2 - Going from Unsupervised to Supervised Deep Learning
+# Part 2 - Going from Unsupervised to Unsupervised - Detecting the probability of frauds
 
 # Creating the matrix of features
 customers = dataset.iloc[:, 1:].values
@@ -58,36 +55,30 @@ customers = dataset.iloc[:, 1:].values
 # Creating the dependent variable
 is_fraud = np.zeros(len(dataset))
 for i in range(len(dataset)):
-    if dataset.iloc[i,0] in frauds:
+    if dataset.iloc[i, 0] in frauds:
         is_fraud[i] = 1
-
+        
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 customers = sc.fit_transform(customers)
 
-# Part 2 - Now let's make the ANN!
-
-# Importing the Keras libraries and packages
+# Building an ANN
+# Import Keras library and packages
+# import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-# Initialising the ANN
-classifier = Sequential()
+# Adding the layers
+classifier = Sequential()                                   
+classifier.add(Dense(2, kernel_initializer='uniform', activation='relu', input_shape=(15,)))
+classifier.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))   
 
-# Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 2, kernel_initializer = 'uniform', activation = 'relu', input_dim = 15))
+# Compiling and fitting
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+classifier.fit(customers, is_fraud, batch_size=1, epochs=2)
 
-# Adding the output layer
-classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
-
-# Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# Fitting the ANN to the Training set
-classifier.fit(customers, is_fraud, batch_size = 1, epochs = 2)
-
-# Predicting the probabilities of frauds
+# Predicting the probabilities of fraud
 y_pred = classifier.predict(customers)
-y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred), axis = 1)
-y_pred = y_pred[y_pred[:, 1].argsort()]
+y_pred = np.concatenate((dataset.iloc[:,0:1].values, y_pred), axis=1)           #axis=1 horizontal concatenation
+y_pred = y_pred[y_pred[:,1].argsort()]
